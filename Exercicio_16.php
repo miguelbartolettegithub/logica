19643b0d4efbe1329e91af5609a9adbfd4e9ca28
Miguel Bartolette@Miguel-PC-Dell <miguel.bartolette@usp.br>
2019-02-08 16:28:53 -0200

    programa fibonacci

--- Exercicio_16.php
+++ Exercicio_16.php
@@ -9,7 +9,7 @@ echo "Fibonacci 2 = " . "$fibonaccis[1]" . PHP_EOL;
 
 for ($i = 2; $i < 10; $i++) {
 	$fibonaccis[$i] = $fibonaccis[$i - 1] + $fibonaccis[$i - 2];
-	echo "Fibonacci $i + 1 = $fibonaccis[$i]" . PHP_EOL;
+	echo "Fibonacci " , ($i + 1), " = $fibonaccis[$i]" , PHP_EOL;
 }
 
 ?>
